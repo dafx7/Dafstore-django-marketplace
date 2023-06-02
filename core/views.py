b960f442226a7c198e2dfaa1ae257b68cb80ce3c
Dafnex <dafnex@gmail.com>
2023-06-02 16:39:04 +0700

    Add Item model and make the front page of the site

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    context = {
+        'categories': Category.objects.all(),        
+        'items': Item.objects.filter(is_sold=False)[0:6],
+    }
+    return render(request, 'core/index.html', context)
 
 
 def contact(request):
