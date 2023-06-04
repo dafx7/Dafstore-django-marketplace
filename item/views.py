832861b15b4f4ab0f11ee10e4f354fd5c1574edd
Dafnex <dafnex@gmail.com>
2023-06-04 15:32:36 +0700

    Add the item detail page

--- item/views.py
+++ item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
+
 
 # Create your views here.
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    context = {
+        'item': item,
+        'related_items': Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3],
+    }
+    return render(request, 'item/details.html', context)
+    
