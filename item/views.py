9a410e93e6c738c2aef06b9d21d6a7fef34c0f47
Dafnex <dafnex@gmail.com>
2023-06-06 16:46:26 +0700

    Make the add item feature

--- item/views.py
+++ item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
@@ -16,5 +16,21 @@ def detail(request, pk):
 
 
 @login_required
-def add(request):
-    form = NewItemForm()
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    context = {
+        'form': form,
+        'title': 'New item',
+    }
+
+    return render(request, 'item/form.html', context)
