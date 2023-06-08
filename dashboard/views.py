0290e3fe661245083fe809b5fa5dbb1fe74ab50d
Dafnex <dafnex@gmail.com>
2023-06-08 10:16:57 +0700

    Add delete item feature

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from item.models import Item
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+
+    context = {
+        'items': items
+    }
+
+    return render(request, 'dashboard/index.html', context)
--- item/views.py
+++ item/views.py
@@ -34,3 +34,11 @@ def new(request):
     }
 
     return render(request, 'item/form.html', context)
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
