78606c41ea0a08e7f52be7cd52c584e7ffe1bd03
Dafnex <dafnex@gmail.com>
2023-07-07 02:25:48 +0700

    Add a logout button

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,14 @@ from . import views
 from .forms import LoginForm
 
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 app_name = 'core'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(authentication_form=LoginForm, template_name='core/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='core:index'), name='logout'),
 
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
